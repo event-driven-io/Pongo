855b76f266712983f78f00a72d901df988df5994
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-07-09 12:03:59 +0200

    Added implementation of insertMany, updateMany and deleteMany

--- src/packages/pongo/src/postgres/postgresCollection.ts
+++ src/packages/pongo/src/postgres/postgresCollection.ts
@@ -1,12 +1,15 @@
 import pg from 'pg';
+import format from 'pg-format';
 import { v4 as uuid } from 'uuid';
 import {
   type PongoCollection,
   type PongoDeleteResult,
   type PongoFilter,
+  type PongoInsertManyResult,
   type PongoInsertOneResult,
   type PongoUpdate,
   type PongoUpdateResult,
+  type WithId,
 } from '../main';
 import { executeSQL } from './execute';
 import { constructFilterQuery } from './filter';
@@ -29,13 +32,29 @@ export const postgresCollection = <T>(
     insertOne: async (document: T): Promise<PongoInsertOneResult> => {
       await createCollection;
 
-      const id = uuid();
+      const _id = uuid();
 
-      const result = await execute(SqlFor.insertOne(id, document));
+      const result = await execute(SqlFor.insertOne({ _id, ...document }));
 
       return result.rowCount
-        ? { insertedId: id, acknowledged: true }
+        ? { insertedId: _id, acknowledged: true }
         : { insertedId: null, acknowledged: false };
+    },
+    insertMany: async (documents: T[]): Promise<PongoInsertManyResult> => {
+      await createCollection;
+
+      const rows = documents.map((doc) => ({
+        _id: uuid(),
+        ...doc,
+      }));
+
+      const result = await execute(SqlFor.insertMany(rows));
+
+      return {
+        acknowledged: result.rowCount === rows.length,
+        insertedCount: result.rowCount ?? 0,
+        insertedIds: rows.map((d) => d._id),
+      };
     },
     updateOne: async (
       filter: PongoFilter<T>,
@@ -48,10 +67,29 @@ export const postgresCollection = <T>(
         ? { acknowledged: true, modifiedCount: result.rowCount }
         : { acknowledged: false, modifiedCount: 0 };
     },
+    updateMany: async (
+      filter: PongoFilter<T>,
+      update: PongoUpdate<T>,
+    ): Promise<PongoUpdateResult> => {
+      await createCollection;
+
+      const result = await execute(SqlFor.updateMany(filter, update));
+      return result.rowCount
+        ? { acknowledged: true, modifiedCount: result.rowCount }
+        : { acknowledged: false, modifiedCount: 0 };
+    },
     deleteOne: async (filter: PongoFilter<T>): Promise<PongoDeleteResult> => {
       await createCollection;
 
       const result = await execute(SqlFor.deleteOne(filter));
+      return result.rowCount
+        ? { acknowledged: true, deletedCount: result.rowCount }
+        : { acknowledged: false, deletedCount: 0 };
+    },
+    deleteMany: async (filter: PongoFilter<T>): Promise<PongoDeleteResult> => {
+      await createCollection;
+
+      const result = await execute(SqlFor.deleteMany(filter));
       return result.rowCount
         ? { acknowledged: true, deletedCount: result.rowCount }
         : { acknowledged: false, deletedCount: 0 };
@@ -77,13 +115,19 @@ export const collectionSQLBuilder = (collectionName: string) => ({
       'CREATE TABLE IF NOT EXISTS %I (_id UUID PRIMARY KEY, data JSONB)',
       collectionName,
     ),
-  insertOne: <T>(id: string, document: T): SQL =>
+  insertOne: <T>(document: WithId<T>): SQL =>
     sql(
       'INSERT INTO %I (_id, data) VALUES (%L, %L)',
       collectionName,
-      id,
-      JSON.stringify({ ...document, _id: id }),
+      document._id,
+      JSON.stringify(document),
     ),
+  insertMany: <T>(documents: WithId<T>[]): SQL => {
+    const values = documents
+      .map((doc) => format('(%L, %L)', doc._id, JSON.stringify(doc)))
+      .join(', ');
+    return sql('INSERT INTO %I (_id, data) VALUES %s', collectionName, values);
+  },
   updateOne: <T>(filter: PongoFilter<T>, update: PongoUpdate<T>): SQL => {
     const filterQuery = constructFilterQuery(filter);
     const updateQuery = buildUpdateQuery(update);
@@ -95,7 +139,22 @@ export const collectionSQLBuilder = (collectionName: string) => ({
       filterQuery,
     );
   },
+  updateMany: <T>(filter: PongoFilter<T>, update: PongoUpdate<T>): SQL => {
+    const filterQuery = constructFilterQuery(filter);
+    const updateQuery = buildUpdateQuery(update);
+
+    return sql(
+      'UPDATE %I SET data = %s WHERE %s',
+      collectionName,
+      updateQuery,
+      filterQuery,
+    );
+  },
   deleteOne: <T>(filter: PongoFilter<T>): SQL => {
+    const filterQuery = constructFilterQuery(filter);
+    return sql('DELETE FROM %I WHERE %s', collectionName, filterQuery);
+  },
+  deleteMany: <T>(filter: PongoFilter<T>): SQL => {
     const filterQuery = constructFilterQuery(filter);
     return sql('DELETE FROM %I WHERE %s', collectionName, filterQuery);
   },
