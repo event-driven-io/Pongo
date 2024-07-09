c43a5617b546bc9ed87f5a868a855708dc42e5df
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-07-09 13:39:42 +0200

    Added missing references to types

--- src/packages/pongo/src/mongo/index.ts
+++ src/packages/pongo/src/mongo/index.ts
@@ -2,3 +2,4 @@ export * from './findCursor';
 export * from './mongoClient';
 export * from './mongoCollection';
 export * from './mongoDb';
+export * from './typing';
--- src/packages/pongo/src/postgres/index.ts
+++ src/packages/pongo/src/postgres/index.ts
@@ -2,4 +2,6 @@ export * from './client';
 export * from './execute';
 export * from './filter';
 export * from './pool';
+export * from './postgresCollection';
+export * from './sql';
 export * from './update';
