2f62c394ce224a1264abf93262c74a7d8d6d730e
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-07-12 18:46:19 +0200

    Added rawSql method to Dumbo

--- src/packages/dumbo/src/sql/index.ts
+++ src/packages/dumbo/src/sql/index.ts
@@ -5,3 +5,7 @@ export type SQL = string & { __brand: 'sql' };
 export const sql = (sqlQuery: string, ...params: unknown[]): SQL => {
   return format(sqlQuery, ...params) as SQL;
 };
+
+export const rawSql = (sqlQuery: string): SQL => {
+  return sqlQuery as SQL;
+};
