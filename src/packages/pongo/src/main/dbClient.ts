4244779203b238ea5088ae86a03a0853e2b5abb0
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-07-30 13:31:35 +0200

    Added typed DbClient options making the first step towards supporting multiple database types

--- src/packages/pongo/src/main/dbClient.ts
+++ src/packages/pongo/src/main/dbClient.ts
@@ -1,13 +1,39 @@
-import { postgresClient, type PongoClientOptions } from '../postgres';
+import {
+  isPostgresClientOptions,
+  postgresDbClient,
+  type PostgresDbClientOptions,
+} from '../postgres';
 import type { PongoCollection, PongoDocument } from './typing/operations';
 
-export interface DbClient {
+export type PongoDbClientOptions<
+  DbType extends string = string,
+  Additional = unknown,
+> = {
+  type: DbType;
+  connectionString: string;
+  dbName: string | undefined;
+} & Additional;
+
+export interface DbClient<
+  DbClientOptions extends PongoDbClientOptions = PongoDbClientOptions,
+> {
+  options: DbClientOptions;
   connect(): Promise<void>;
   close(): Promise<void>;
   collection: <T extends PongoDocument>(name: string) => PongoCollection<T>;
 }
 
-export const getDbClient = (options: PongoClientOptions): DbClient => {
+export type AllowedDbClientOptions = PostgresDbClientOptions;
+
+export const getDbClient = <
+  DbClientOptions extends AllowedDbClientOptions = AllowedDbClientOptions,
+>(
+  options: DbClientOptions,
+): DbClient<DbClientOptions> => {
+  const { type } = options;
   // This is the place where in the future could come resolution of other database types
-  return postgresClient(options);
+  if (!isPostgresClientOptions(options))
+    throw new Error(`Unsupported db type: ${type}`);
+
+  return postgresDbClient(options) as DbClient<DbClientOptions>;
 };
