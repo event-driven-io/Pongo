5fe9680b1866360bde918604fc710fa687a0a0d0
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-08-06 13:58:42 +0200

    Cleaned up connection setup by wrapping it with factory method
    Fixes #53

--- src/packages/dumbo/src/core/connections/pool.ts
+++ src/packages/dumbo/src/core/connections/pool.ts
@@ -1,6 +1,9 @@
-import type { WithSQLExecutor } from '../execute';
+import { sqlExecutorInNewConnection, type WithSQLExecutor } from '../execute';
 import { type Connection } from './connection';
-import type { DatabaseTransactionFactory } from './transaction';
+import {
+  transactionFactoryWithNewConnection,
+  type DatabaseTransactionFactory,
+} from './transaction';
 
 export interface ConnectionPool<ConnectionType extends Connection = Connection>
   extends WithSQLExecutor,
@@ -10,7 +13,45 @@ export interface ConnectionPool<ConnectionType extends Connection = Connection>
   close: () => Promise<void>;
 }
 
-export type ConnectionPoolProvider<
+export type ConnectionPoolFactory<
   ConnectionPoolType extends ConnectionPool = ConnectionPool,
   ConnectionPoolOptions = unknown,
 > = (options: ConnectionPoolOptions) => ConnectionPoolType;
+
+export const createConnectionPool = <
+  ConnectionType extends Connection,
+  ConnectionPoolType extends ConnectionPool<ConnectionType>,
+>(
+  pool: Pick<ConnectionPool<ConnectionType>, 'type'> &
+    Partial<ConnectionPool<ConnectionType>> & {
+      getConnection: () => ConnectionType;
+    },
+): ConnectionPoolType => {
+  const { type, getConnection } = pool;
+
+  const open =
+    'open' in pool ? pool.open : () => Promise.resolve(getConnection());
+
+  const close = 'close' in pool ? pool.close : () => Promise.resolve();
+
+  const execute =
+    'execute' in pool ? pool.execute : sqlExecutorInNewConnection({ open });
+
+  const transaction =
+    'transaction' in pool && 'withTransaction' in pool
+      ? {
+          transaction: pool.transaction,
+          withTransaction: pool.withTransaction,
+        }
+      : transactionFactoryWithNewConnection(getConnection);
+
+  const result: ConnectionPool<ConnectionType> = {
+    type,
+    open,
+    close,
+    execute,
+    ...transaction,
+  };
+
+  return result as ConnectionPoolType;
+};
