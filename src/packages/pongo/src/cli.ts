02e559dc5a4241e1f23e1f9da67789ad7569842a
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-09-12 10:36:27 +0200

    Added command to generate or print sample config file

--- src/packages/pongo/src/cli.ts
+++ src/packages/pongo/src/cli.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { migrateCommand } from './commandLine';
+import { configCommand, migrateCommand } from './commandLine';
 
 const program = new Command();
 
 program.name('pongo').description('CLI tool for Pongo');
 
+program.addCommand(configCommand);
 program.addCommand(migrateCommand);
 
 program.parse(process.argv);
