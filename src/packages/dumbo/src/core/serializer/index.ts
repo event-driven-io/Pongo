eccc8c4256850b4ee7a0a714212a12c6e96fbc4a
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-10-01 12:43:59 +0200

    Added Pongo shell

--- src/packages/dumbo/src/core/serializer/index.ts
+++ src/packages/dumbo/src/core/serializer/index.ts
@@ -1,4 +1,4 @@
-interface Serializer<
+export interface Serializer<
   Payload,
   SerializeOptions = never,
   DeserializeOptions = SerializeOptions,
@@ -7,10 +7,9 @@ interface Serializer<
   deserialize<T>(payload: Payload, options?: DeserializeOptions): T;
 }
 
-interface ObjectCodec<T, Payload> {
+export interface ObjectCodec<T, Payload> {
   encode(object: T): Payload;
   decode(payload: Payload): T;
 }
 
 export * from './json';
-export { type ObjectCodec, type Serializer };
