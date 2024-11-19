event-driven-io/Pongo

commit 202433bf4a3f7c043dea7534ecfc91a3b8737cc6

Bumped to 0.16.2

src/packages/pongo/src/commandLine/shell.ts
@@ -124,7 +124,7 @@ const startRepl = async (options: {
   setLogLevel(process.env.DUMBO_LOG_LEVEL ?? options.logging.logLevel);
   setLogStyle(process.env.DUMBO_LOG_STYLE ?? options.logging.logStyle);
 
-  console.log(chalk.green('Starting Pongo Shell (version: 0.16.1)'));
+  console.log(chalk.green('Starting Pongo Shell (version: 0.16.2)'));
 
   if (options.logging.printOptions) {
     console.log(chalk.green('With Options:'));

commit 6ef3ce47e7052e02c96824c5b3919ff0b6d8c376

Handle method passes again null instead of empty object when doucument doesn't exists
This fixes regression introduced by #99

src/packages/pongo/src/commandLine/shell.ts
@@ -124,7 +124,7 @@ const startRepl = async (options: {
   setLogLevel(process.env.DUMBO_LOG_LEVEL ?? options.logging.logLevel);
   setLogStyle(process.env.DUMBO_LOG_STYLE ?? options.logging.logStyle);
 
-  console.log(chalk.green('Starting Pongo Shell (version: 0.16.2)'));
+  console.log(chalk.green('Starting Pongo Shell (version: 0.16.3)'));
 
   if (options.logging.printOptions) {
     console.log(chalk.green('With Options:'));

src/packages/pongo/src/core/collection/pongoCollection.ts
@@ -348,7 +348,7 @@ export const pongoCollection = <
       const existing = (await collection.findOne(
         byId,
         options,
-      )) as WithVersion<T>;
+      )) as WithVersion<T> | null;
 
       const expectedVersion = expectedVersionValue(version);
 
@@ -369,13 +369,15 @@ export const pongoCollection = <
         );
       }
 
-      const result = await handle({ ...existing } as T);
+      const result = await handle(
+        existing !== null ? ({ ...existing } as T) : null,
+      );
 
-      if (deepEquals(existing as T, result))
+      if (deepEquals(existing as T | null, result))
         return operationResult<PongoHandleResult<T>>(
           {
             successful: true,
-            document: existing as T,
+            document: existing as T | null,
           },
           { operationName: 'handle', collectionName, errors },
         );

src/packages/pongo/src/e2e/postgres.e2e.spec.ts
@@ -1061,6 +1061,25 @@ void describe('MongoDB Compatibility Tests', () => {
   });
 
   void describe('Handle Operations', () => {
+    void it(`should pass null to handle if document doesn't exist`, async () => {
+      const pongoCollection = pongoDb.collection<User>('handleCollection');
+      const nonExistingId = uuid() as unknown as ObjectId;
+
+      const newDoc: User = { name: 'John', age: 25 };
+
+      let wasHandled = false;
+
+      const handle = (existing: User | null) => {
+        wasHandled = true;
+        assert.equal(existing, null);
+        return newDoc;
+      };
+
+      await pongoCollection.handle(nonExistingId, handle);
+
+      assert.ok(wasHandled);
+    });
+
     void it('should insert a new document if it does not exist', async () => {
       const pongoCollection = pongoDb.collection<User>('handleCollection');
       const nonExistingId = uuid() as unknown as ObjectId;