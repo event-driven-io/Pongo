e8fc1a5772f90b1cc94bbfb56e1af3ac2f9e58c0
dave <dave.devs.stuff@gmail.com>
2025-07-04 22:29:28 +0100

    Adjusted the methods inside of begin and commit, and let the transaction handle the try catch. Added decrement for rollback so it only gets call at the very end

--- src/packages/dumbo/src/storage/sqlite/core/transactions/index.ts
+++ src/packages/dumbo/src/storage/sqlite/core/transactions/index.ts
@@ -33,16 +33,8 @@ export const sqliteTransaction =
       const client = await getClient;
 
       if (transactionCounter.level >= 1) {
-        try {
-          transactionCounter.increment();
-          await client.query(
-            `SAVEPOINT transaction${transactionCounter.level}`,
-          );
-        } catch (error) {
-          console.log('Rolling back begin commit');
-          await this.rollback(error);
-          throw error;
-        }
+        transactionCounter.increment();
+        await client.query(`SAVEPOINT transaction${transactionCounter.level}`);
         return;
       }
 
@@ -53,13 +45,8 @@ export const sqliteTransaction =
       const client = await getClient;
 
       if (transactionCounter.level > 1) {
-        try {
-          await client.query(`RELEASE transaction${transactionCounter.level}`);
-          transactionCounter.decrement();
-        } catch (error) {
-          console.log(error);
-          await this.rollback(error);
-        }
+        await client.query(`RELEASE transaction${transactionCounter.level}`);
+        transactionCounter.decrement();
 
         return;
       }
@@ -71,7 +58,11 @@ export const sqliteTransaction =
     },
     rollback: async function (error?: unknown) {
       const client = await getClient;
-      transactionCounter.reset();
+      if (transactionCounter.level > 1) {
+        transactionCounter.decrement();
+        return;
+      }
+
       await client.query('ROLLBACK');
 
       if (options?.close) await options?.close(client, error);
