82577b970816a799820a656ab54960b013e3a16b
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-08-14 16:36:11 +0200

    Added Storage Plugin and Storage Plugin registry together with example pgPlugin

--- src/packages/dumbo/src/storage/postgresql/pg/index.ts
+++ src/packages/dumbo/src/storage/postgresql/pg/index.ts
@@ -1,11 +1,44 @@
 import type { Dumbo } from '../../../core';
 import {
+  pluginRegistry,
+  type StoragePlugin,
+} from '../../../core/plugins/storagePlugin';
+import { DefaultPostgreSQLMigratorOptions, pgFormatter } from '../core';
+import {
   type NodePostgresConnection,
   type NodePostgresConnector,
+  NodePostgresConnectorType,
   type NodePostgresPool,
   nodePostgresPool,
   type NodePostgresPoolOptions,
 } from './connections';
+
+const pgStoragePlugin: StoragePlugin<
+  NodePostgresConnector,
+  NodePostgresConnection
+> = {
+  connector: NodePostgresConnectorType,
+  createPool: (options) =>
+    nodePostgresPool(options as unknown as PostgresPoolOptions),
+  sqlFormatter: pgFormatter,
+  defaultMigratorOptions: DefaultPostgreSQLMigratorOptions,
+};
+
+pluginRegistry.register(NodePostgresConnectorType, pgStoragePlugin);
+
+export const dumbo = <
+  DumboOptionsType extends PostgresPoolOptions = PostgresPoolOptions,
+>(
+  options: DumboOptionsType,
+): Dumbo<NodePostgresConnector> => nodePostgresPool(options);
+
+export * from './connections';
+export * from './execute';
+export * from './serialization';
+
+export { pgStoragePlugin as storagePlugin };
+
+// TODO: Remove stuff below
 
 export type PostgresConnector = NodePostgresConnector;
 export type PostgresPool = NodePostgresPool;
@@ -15,13 +48,3 @@ export type PostgresPoolOptions = NodePostgresPoolOptions;
 export const postgresPool = nodePostgresPool;
 
 export const connectionPool = postgresPool;
-
-export const dumbo = <
-  DumboOptionsType extends PostgresPoolOptions = PostgresPoolOptions,
->(
-  options: DumboOptionsType,
-): Dumbo<NodePostgresConnector> => connectionPool(options);
-
-export * from './connections';
-export * from './execute';
-export * from './serialization';
