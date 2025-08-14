db6c19a9080871a2e8e4c68499b2924034944f99
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-08-14 16:42:35 +0200

    Added sqlite3 plugin

--- src/packages/dumbo/src/storage/sqlite/sqlite3/index.ts
+++ src/packages/dumbo/src/storage/sqlite/sqlite3/index.ts
@@ -1,16 +1,36 @@
 export * from './connections';
 import type { Dumbo } from '../../../core';
 import {
+  pluginRegistry,
+  type StoragePlugin,
+} from '../../../core/plugins/storagePlugin';
+import {
+  DefaultSQLiteMigratorOptions,
+  sqliteFormatter,
   sqlitePool,
   type SQLiteConnection,
   type SQLitePoolOptions,
 } from '../core';
 import {
+  SQLite3ConnectorType,
   sqlite3Client as sqliteClient,
   type SQLite3Connector,
 } from './connections';
 
-export { sqliteClient };
+const sqlite3StoragePlugin: StoragePlugin<
+  SQLite3Connector,
+  SQLiteConnection<SQLite3Connector>
+> = {
+  connector: SQLite3ConnectorType,
+  createPool: (options) =>
+    sqlitePool(options as unknown as SQLitePoolOptions<SQLite3Connector>),
+  sqlFormatter: sqliteFormatter,
+  defaultMigratorOptions: DefaultSQLiteMigratorOptions,
+};
+
+pluginRegistry.register(SQLite3ConnectorType, sqlite3StoragePlugin);
+
+export { sqliteClient, sqlite3StoragePlugin as storagePlugin };
 
 export const connectionPool = sqlitePool;
 
