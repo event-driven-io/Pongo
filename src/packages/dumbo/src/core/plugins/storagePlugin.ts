135a603d5a863b1f5dc09585951ed2421daf2f09
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-08-14 16:36:11 +0200

    Added Storage Plugin and Storage Plugin registry together with example pgPlugin

---
+++ src/packages/dumbo/src/core/plugins/storagePlugin.ts
@@ -0,0 +1,82 @@
+import { tracer, type Dumbo, type DumboConnectionOptions } from '..';
+import type { Connection } from '../connections';
+import type { ConnectorType } from '../connectors';
+import type { MigratorOptions } from '../schema';
+import type { SQLFormatter } from '../sql';
+
+export interface StoragePlugin<
+  Connector extends ConnectorType = ConnectorType,
+  ConnectionType extends Connection<Connector> = Connection<Connector>,
+> {
+  readonly connector: Connector;
+
+  createPool(options: DumboConnectionOptions): Dumbo<Connector, ConnectionType>;
+
+  readonly sqlFormatter: SQLFormatter;
+
+  readonly defaultMigratorOptions: MigratorOptions;
+}
+
+export const PluginRegistry = () => {
+  const plugins = new Map<
+    ConnectorType,
+    StoragePlugin | (() => Promise<StoragePlugin>)
+  >();
+
+  return {
+    register: <
+      Connector extends ConnectorType = ConnectorType,
+      ConnectionType extends Connection<Connector> = Connection<Connector>,
+    >(
+      connector: Connector,
+      plugin:
+        | StoragePlugin<Connector, ConnectionType>
+        | (() => Promise<StoragePlugin<Connector, ConnectionType>>),
+    ): void => {
+      if (plugins.has(connector)) {
+        tracer.warn(`Plugin already registered for connector: ${connector}`);
+      }
+      plugins.set(connector, plugin);
+    },
+
+    tryResolve: async <
+      Connector extends ConnectorType = ConnectorType,
+      ConnectionType extends Connection<Connector> = Connection<Connector>,
+    >(
+      connector: ConnectorType,
+    ): Promise<StoragePlugin<Connector, ConnectionType> | null> => {
+      const entry = plugins.get(connector);
+      if (!entry) {
+        return null;
+      }
+
+      if (typeof entry === 'function') {
+        const plugin = await entry();
+        plugins.set(connector, plugin);
+        return plugin as unknown as StoragePlugin<Connector, ConnectionType>;
+      }
+
+      return entry as unknown as StoragePlugin<Connector, ConnectionType>;
+    },
+
+    tryGet: <
+      Connector extends ConnectorType = ConnectorType,
+      ConnectionType extends Connection<Connector> = Connection<Connector>,
+    >(
+      connector: ConnectorType,
+    ): StoragePlugin<Connector, ConnectionType> | null => {
+      const entry = plugins.get(connector);
+      return typeof entry !== 'function'
+        ? (entry as unknown as StoragePlugin<Connector, ConnectionType>)
+        : null;
+    },
+
+    get connectorTypes(): ConnectorType[] {
+      return Array.from(plugins.keys());
+    },
+
+    has: (connector: ConnectorType): boolean => plugins.has(connector),
+  };
+};
+
+export const pluginRegistry = PluginRegistry();
