0c8f1282ef4344db3ec0d746571cda8f4bce9460
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-09-12 14:41:25 +0200

    Unfied handling of the migration table

--- src/packages/dumbo/src/storage/postgresql/core/schema/migrations.ts
+++ src/packages/dumbo/src/storage/postgresql/core/schema/migrations.ts
@@ -1,53 +1,13 @@
 import {
-  type DatabaseLockOptions,
   type MigratorOptions,
-  type SchemaComponent,
-  MIGRATIONS_LOCK_ID,
-  SQL,
   registerDefaultMigratorOptions,
-  schemaComponent,
-  sqlMigration,
 } from '../../../../core';
 import { AdvisoryLock } from '../locks';
 
-const { AutoIncrement, Varchar, Timestamp } = SQL.column.type;
-
-const migrationTableSQL = SQL`
-  CREATE TABLE IF NOT EXISTS migrations (
-    id ${AutoIncrement({ primaryKey: true })},
-    name ${Varchar(255)} NOT NULL UNIQUE,
-    application ${Varchar(255)} NOT NULL DEFAULT 'default',
-    sql_hash ${Varchar(64)} NOT NULL,
-    timestamp ${Timestamp} NOT NULL DEFAULT CURRENT_TIMESTAMP
-  );
-`;
-
-export const migrationTableSchemaComponent = schemaComponent(
-  'dumbo:schema-component:migrations-table',
-  {
-    migrations: [sqlMigration('dumbo:migrationTable:001', [migrationTableSQL])],
-  },
-);
-
 export const DefaultPostgreSQLMigratorOptions: MigratorOptions = {
-  schema: {
-    migrationTable: migrationTableSchemaComponent,
-  },
   lock: {
     databaseLock: AdvisoryLock,
-    options: {
-      lockId: MIGRATIONS_LOCK_ID,
-    },
   },
-};
-
-export type PostgreSQLMigratorOptions = {
-  schema?: Omit<SchemaComponent, 'migrationTable'>;
-  lock?: {
-    options?: Omit<DatabaseLockOptions, 'lockId'> &
-      Partial<Pick<DatabaseLockOptions, 'lockId'>>;
-  };
-  dryRun?: boolean | undefined;
 };
 
 registerDefaultMigratorOptions('PostgreSQL', DefaultPostgreSQLMigratorOptions);
--- src/packages/dumbo/src/storage/sqlite/core/schema/migrations.ts
+++ src/packages/dumbo/src/storage/sqlite/core/schema/migrations.ts
@@ -1,75 +1,8 @@
 import {
-  MIGRATIONS_LOCK_ID,
-  NoDatabaseLock,
   registerDefaultMigratorOptions,
-  schemaComponent,
-  SQL,
-  sqlMigration,
-  type DatabaseLockOptions,
   type MigratorOptions,
-  type SchemaComponent,
 } from '../../../../core';
 
-const { AutoIncrement, Varchar, Timestamp } = SQL.column.type;
-
-const migrationTableSQL = SQL`
-  CREATE TABLE IF NOT EXISTS migrations (
-    id ${AutoIncrement({ primaryKey: true })},
-    name ${Varchar(255)} NOT NULL UNIQUE,
-    application ${Varchar(255)} NOT NULL DEFAULT 'default',
-    sql_hash ${Varchar(64)} NOT NULL,
-    timestamp ${Timestamp} NOT NULL DEFAULT CURRENT_TIMESTAMP
-  );
-`;
-
-export const migrationTableSchemaComponent = schemaComponent(
-  'dumbo:schema-component:migrations-table',
-  {
-    migrations: [sqlMigration('dumbo:migrationTable:001', [migrationTableSQL])],
-  },
-);
-
-export const DefaultSQLiteMigratorOptions: MigratorOptions = {
-  schema: {
-    migrationTable: migrationTableSchemaComponent,
-  },
-  lock: {
-    databaseLock: NoDatabaseLock,
-    options: {
-      lockId: MIGRATIONS_LOCK_ID,
-    },
-  },
-};
-
-export type SQLiteMigratorOptions = {
-  schema?: Omit<SchemaComponent, 'migrationTable'>;
-  lock?: {
-    options?: Omit<DatabaseLockOptions, 'lockId'> &
-      Partial<Pick<DatabaseLockOptions, 'lockId'>>;
-  };
-  dryRun?: boolean | undefined;
-};
-
-export const SQLiteMigratorOptions = (
-  options?: SQLiteMigratorOptions,
-): MigratorOptions => {
-  const defaultOptions = DefaultSQLiteMigratorOptions;
-  return {
-    ...defaultOptions,
-    schema: {
-      ...defaultOptions.schema,
-      ...(options?.schema ?? {}),
-    },
-    lock: {
-      ...defaultOptions.lock,
-      options: {
-        ...defaultOptions.lock.options,
-        ...(options ?? {}),
-        lockId: MIGRATIONS_LOCK_ID,
-      },
-    },
-    dryRun: defaultOptions.dryRun ?? options?.dryRun,
-  };
-};
+export const DefaultSQLiteMigratorOptions: MigratorOptions = {};
 
 registerDefaultMigratorOptions('SQLite', DefaultSQLiteMigratorOptions);
