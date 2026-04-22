45b5b7286d4bce83cbaf0809379acae4e25d8b90
Oskar Dudycz <oskar.dudycz@gmail.com>
2026-04-22 10:52:52 +0200

    Updated samples to TS 6 and ESlint 10

--- samples/simple-ts/eslint.config.mjs
+++ samples/simple-ts/eslint.config.mjs
@@ -1,19 +1,8 @@
-import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 
 export default defineConfig([
   globalIgnores([
@@ -23,26 +12,15 @@ export default defineConfig([
     'src/types/',
     '!**/.eslintrc.js',
   ]),
+  js.configs.recommended,
+  ...typescriptEslint.configs['flat/recommended'],
+  ...typescriptEslint.configs['flat/recommended-type-checked'],
+  prettierRecommended,
   {
-    extends: compat.extends(
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      'plugin:prettier/recommended',
-    ),
-
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-    },
-
     languageOptions: {
       globals: {
         ...globals.node,
       },
-
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
 
       parserOptions: {
         project: './tsconfig.json',
